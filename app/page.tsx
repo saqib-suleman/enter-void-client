13c9aa0c763cd21aff71358b4e75965250810a67
saqib-suleman <saqibs3291@gmail.com>
2024-12-26 06:26:10 +0500

    add husky

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { HELLO } from "./queries";
 
 export default function Home() {
   const { data } = useQuery(HELLO);
+  const da = 1;
 
   return <div className="h-full w-full">{JSON.stringify(data)}</div>;
 }
